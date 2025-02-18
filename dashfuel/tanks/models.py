d15f01bb5f217977d4e6aa7f0d3f102139de3695
Clark Burns <rclarkburns@icloud.com>
2025-02-18 13:50:14 -0700

    [R] Cleaning up

--- dashfuel/tanks/models.py
+++ dashfuel/tanks/models.py
@@ -69,74 +69,61 @@ class WeeklyTankSales(models.Model):
 @receiver(post_save, sender=TankVolume)
 def update_weekly_sales(sender, instance, created, **kwargs):
     if not created:
-        weekly_sales = WeeklyTankSales.objects.filter(tank=instance.tank)
-        updates = []
-
-        for sales_record in weekly_sales:
-            week_end = sales_record.week_end
-
-            week_volumes = TankVolume.objects.filter(
-                tank=instance.tank,
-                created_at__date__range=[
-                    week_end - timezone.timedelta(days=7),
-                    week_end
-                ]
-            ).order_by('created_at', 'id')
-
-            total_sales = 0
-            total_sales_count = 0
-
-            for day_offset, day_field in sales_record.day_field_mapping.items():
-                day_date = week_end - timezone.timedelta(days=6-day_offset)
-                prev_date = day_date - timezone.timedelta(days=1)
-
-                day_volumes = week_volumes.filter(created_at__date=day_date)
-                prev_day_volumes = week_volumes.filter(created_at__date=prev_date)
-
-                daily_sales = 0
-                if prev_day_volumes.exists() and day_volumes.exists():
-                    prev_day_last_volume = prev_day_volumes.last().volume
-
-
-                    current_volumes = list(day_volumes)
-                    for i in range(len(current_volumes)):
-                        if i == 0:
-                            if current_volumes[i].volume != prev_day_last_volume:
-                                volume_difference = prev_day_last_volume - current_volumes[i].volume
-                                if volume_difference >= 0:
-                                    daily_sales += volume_difference
-                                    total_sales_count += 1
-                                    total_sales += daily_sales
-                        else:
-                            volume_difference = current_volumes[i-1].volume - current_volumes[i].volume
-                            if volume_difference >= 0:
-                                daily_sales += volume_difference
-                                total_sales_count += 1
-                                total_sales += daily_sales
-
-                    setattr(sales_record, day_field, daily_sales)
-                elif day_volumes.exists():
-                    daily_sales = 0
-                    current_volumes = list(day_volumes)
-                    for i in range(len(current_volumes)):
-                        volume_difference = current_volumes[i-1].volume - current_volumes[i].volume
-                        if volume_difference >= 0:
-                            daily_sales += volume_difference
-                            total_sales_count += 1
-                            total_sales += daily_sales
-                    setattr(sales_record, day_field, daily_sales)
-                else:
-                    setattr(sales_record, day_field, 0)
-
-            sales_record.total_sales = total_sales
-            sales_record.total_sales_count = total_sales_count
-            updates.append(sales_record)
-
-        WeeklyTankSales.objects.bulk_update(
-            updates,
-            ['monday_sales', 'tuesday_sales', 'wednesday_sales', 'thursday_sales', 'friday_sales', 'saturday_sales', 'sunday_sales', 'total_sales', 'total_sales_count']
+        current_date = instance.created_at.date()
+        week_start = current_date - timedelta(days=current_date.weekday())
+        week_end = week_start + timedelta(days=6)
+        weekly_tank_sales, _ = WeeklyTankSales.objects.get_or_create(
+            tank=instance.tank,
+            week_start=week_start,
+            week_end=week_end
         )
 
+        week_volumes = TankVolume.objects.filter(
+            tank=instance.tank,
+            created_at__date__range=[
+                week_end - timezone.timedelta(days=7),
+                week_end
+            ]
+        ).order_by('created_at', 'id')
+
+        total_sales = 0
+        total_sales_count = 0
+
+        for day_offset, day_field in weekly_tank_sales.day_field_mapping.items():
+            daily_sales = 0
+            day_date = week_end - timezone.timedelta(days=6-day_offset)
+            prev_date = day_date - timezone.timedelta(days=1)
+            day_volumes = week_volumes.filter(created_at__date=day_date)
+            prev_day_volumes = week_volumes.filter(created_at__date=prev_date)
+
+            if prev_day_volumes.exists() and day_volumes.exists():
+                prev_day_last_volume = prev_day_volumes.last().volume
+                current_volumes = list(day_volumes)
+                for i in range(len(current_volumes)):
+                    if i == 0:
+                        volume_difference = prev_day_last_volume - current_volumes[i].volume
+                    else:
+                        volume_difference = current_volumes[i-1].volume - current_volumes[i].volume
+                    if volume_difference >= 0:
+                        daily_sales += volume_difference
+                        total_sales_count += 1
+                total_sales += daily_sales
+                setattr(weekly_tank_sales, day_field, daily_sales)
+            elif day_volumes.exists():
+                current_volumes = list(day_volumes)
+                for i in range(len(current_volumes)):
+                    volume_difference = current_volumes[i-1].volume - current_volumes[i].volume
+                    if volume_difference >= 0:
+                        daily_sales += volume_difference
+                        total_sales_count += 1
+                total_sales += daily_sales
+                setattr(weekly_tank_sales, day_field, daily_sales)
+            else:
+                setattr(weekly_tank_sales, day_field, 0)
+
+        weekly_tank_sales.total_sales = total_sales
+        weekly_tank_sales.total_sales_count = total_sales_count
+        weekly_tank_sales.save()
     else:
         previous_day = instance.created_at.date() - timedelta(days=1)
         previous_volume = TankVolume.objects.filter(
@@ -159,18 +146,18 @@ def update_weekly_sales(sender, instance, created, **kwargs):
         week_start = current_date - timedelta(days=current_date.weekday())
         week_end = week_start + timedelta(days=6)
 
-        weekly_sales, _ = WeeklyTankSales.objects.get_or_create(
+        weekly_tank_sales, _ = WeeklyTankSales.objects.get_or_create(
             tank=instance.tank,
             week_start=week_start,
             week_end=week_end
         )
 
-        day_field = weekly_sales.day_field_mapping[current_date.weekday()]
-        current_day_sales = getattr(weekly_sales, day_field)
-        setattr(weekly_sales, day_field, current_day_sales + volume_difference)
+        day_field = weekly_tank_sales.day_field_mapping[current_date.weekday()]
+        current_day_sales = getattr(weekly_tank_sales, day_field)
+        setattr(weekly_tank_sales, day_field, current_day_sales + volume_difference)
 
         if volume_difference > 0:
-            weekly_sales.total_sales_count += 1
+            weekly_tank_sales.total_sales_count += 1
 
-        weekly_sales.total_sales += volume_difference
-        weekly_sales.save()
+        weekly_tank_sales.total_sales += volume_difference
+        weekly_tank_sales.save()
